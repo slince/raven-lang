d0bdd58664ccdfd15ceaa0b2a7bba7398e235a95
slince <taosikai@sina.cn>
2022-12-11 23:41:21 +0800

    优化

--- ir/traversal/cfg.go
+++ ir/traversal/cfg.go
@@ -3,11 +3,12 @@ package traversal
 import "github.com/slince/php-plus/ir"
 
 type CfgBuilder struct {
+	block *ir.BasicBlock
 }
 
 func (c *CfgBuilder) EnterNode(inst ir.Instruction) {
 	if inst, ok := inst.(*ir.Label); ok {
-
+		c.block = ir.NewBlock(inst.Name)
 	}
 }
 
